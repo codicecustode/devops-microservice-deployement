fa3177f25098d2a8cd8701214877e8e9cbb228e7
Aman Kumar Singh <amanrkt2020@gmail.com>
2026-08-05 00:43:36 +0530

    refactor(eks): remove EKS cluster resource and update node group instance type to t2.micro

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,23 +1,3 @@
-# 1. EKS Control Plane
-resource "aws_eks_cluster" "main" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
-
-  vpc_config {
-    subnet_ids = [
-      aws_subnet.public_1.id,
-      aws_subnet.public_2.id,
-      aws_subnet.private_1.id,
-      aws_subnet.private_2.id
-    ]
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_policy
-  ]
-}
-
-# 2. Worker Node Group (Runs in private subnets)
 resource "aws_eks_node_group" "nodes" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "main-node-group"
@@ -30,7 +10,7 @@ resource "aws_eks_node_group" "nodes" {
     max_size     = 3
   }
 
-  instance_types = ["t3.medium"]
+  instance_types = ["t2.micro"]
 
   depends_on = [
     aws_iam_role_policy_attachment.node_worker,
