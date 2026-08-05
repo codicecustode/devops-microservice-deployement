3434515bd1fe632f39db9434a4375a52e59ad809
Aman Kumar Singh <amanrkt2020@gmail.com>
2026-08-06 02:14:27 +0530

    refactor(eks): update node group max size and instance type to t3.small

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -27,10 +27,16 @@ resource "aws_eks_node_group" "nodes" {
   scaling_config {
     desired_size = 2
     min_size     = 1
-    max_size     = 3
+    max_size     = 2
   }
 
-  instance_types = ["t2.micro"]
+  # Validated Free Tier allowed instance type for your account
+  instance_types = ["t3.small"]
+  ami_type       = "AL2023_x86_64_STANDARD"
+  capacity_type  = "ON_DEMAND"
+
+  # Disk configuration in GB
+  disk_size = 20
 
   depends_on = [
     aws_iam_role_policy_attachment.node_worker,
